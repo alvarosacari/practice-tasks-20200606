8c8b4c6b5b48810d6a6ae303048cf0d1677d98b9
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2020-06-02 18:28:50 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-layout
+      column
+      justify-center
+      align-center
+    >
+      <v-flex
+        xs12
+        sm8
+        md6
+      >
+        <v-card>
+          <v-card-text>
+            Index page
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+}
+</script>
